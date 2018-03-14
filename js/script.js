749cac5f8ea0eb9e03491e97c0bfe13e3b7ff9dd
Stephan Exius <sexius@westinghousehs.org>
2018-03-14 21:33:27 +0000

    changed html js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,39 @@
 $(document).ready(function(){
   
   
-  
+    function giphyURLWithSearchTerm(searchTerm) {
+
+        var url = "https://api.giphy.com/v1/stickers/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC";
+        return url;
+
+    }
+
+    function appendImageToBody(srcURL) {
+        $('body').append('<img src=' + srcURL + '>');
+    }
+
+    function callGiphyAPIWithSearchTerm(searchTerm) {
+        var url = giphyURLWithSearchTerm(searchTerm);
+        console.log(url);
+        $.ajax({
+            url: url,
+            method: "GET",
+            success: function(response) {
+                var image_url = response.data[0].images.original.url;
+                console.log(image_url);
+                appendImageToBody(image_url);
+            },
+
+        });
+    }
+
+    $('button').click(function() {
+        var searchTerm = $('#srch-term').val();
+        console.log(searchTerm);
+        callGiphyAPIWithSearchTerm(searchTerm);
+
+    });
+
+
   
 });
